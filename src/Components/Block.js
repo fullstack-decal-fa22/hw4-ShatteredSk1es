0e704473975be3126a00f47a5c38776207369ac1
Mohammad Shahnawaz <mohammadshahnawaz088@gmail.com>
2022-11-06 14:36:19 -0800

    Done with this

--- src/Components/Block.js
+++ src/Components/Block.js
@@ -3,15 +3,12 @@ import React from 'react';
 
 const Block = (props) => {
     /* TODO */
-
-
     const colorOfPost = {
         backgroundColor: props.color
     }
 
     return (
         <div data-testid='main-component'>
-
 
             <div className="post" style={colorOfPost}></div>
             <div className="caption" >{props.color}</div>
